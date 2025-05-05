b2eac3c773fab7b03c01dc5cf3800bf8d6636166
Pabllo Leviev <pablloleviev@sempreceub.com>
2025-05-05 19:23:59 -0300

    Delete leviev02.py

--- leviev02.py
+++
@@ -1,6 +0,0 @@
-valor1=print("A subtint(input("Digite o primeiro valor:"))
-valor2=int(input("Digite o segundo valor:"))
-soma=valor1+valor2
-print("A soma é igual a", soma)
-subtracao=valor1-valor2
-print('A subtração é igual a", subtracao)
